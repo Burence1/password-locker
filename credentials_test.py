397d24fea2e2f5e2e446238e07562009be2d01a4
Burence1 <burensomondi@gmail.com>
2021-04-09 20:26:12 +0300

    complete testing logic

--- credentials_test.py
+++ credentials_test.py
@@ -39,6 +39,47 @@ class TestCredentials(unittest.TestCase):
         test_credentials = Credentials ("Twitter", "Moringa", "123r")
         test_credentials.add_credentials()
         self.assertEqual(len(Credentials.credentials_list),2)
+
+      def test_display_credentials(self):
+        '''
+        test case for displaying user's credentials
+        '''
+
+        self.assertEqual(Credentials.credentials_list,Credentials.display_credentials())        
       
+      def test_credentials_exist(self):
+        '''
+        test case for confirming credentials that exist
+        '''
+        self.new_credentials.add_credentials()
+        test_credentials = Credentials("Instagram", "Burence","Br1")
+        test_credentials.add_credentials()
+        
+        credential_exist = Credentials.credentials_exist("Instagram")
+        self.assertTrue(credential_exist)
+
+      def test_search__app_name(self):
+        '''
+        test case for searching app by name
+        '''
+        self.new_credentials.add_credentials()
+        test_credentials = Credentials("Instagram", "Burence", "Br1")
+        test_credentials.add_credentials()
+
+        found_credentials = Credentials.search_app_name("Instagram")
+        self.assertEqual(found_credentials.app_name, test_credentials.app_name)
+
+      def test_delete_credentials(self):
+        '''
+        test case for delete credentials logic
+        '''
+
+        self.new_credentials.add_credentials()
+        test_credentials = Credentials("Instagram", "Burence", "Br1")
+        test_credentials.add_credentials()
+
+        test_credentials.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
 if __name__ == '__main__':
     unittest.main()
